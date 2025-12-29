4cc66c0c05ded1fa21789d0acbd1704c96b827ef
rimhyena <rimhyena@naeileun.dev>
2025-12-29 16:38:22 +0900

    ✨feat: 결제 수단 관련 파일 삭제 - PaymentMethodController, PaymentMethodService, PaymentMethodModule, PaymentMethodDto, PaymentMethod 엔티티 및 관련 열거형 파일 삭제 - 결제 수단 기능을 위한 모든 관련 코드 제거

--- src/payment-method/enum/payment-method-status-type.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum PaymentMethodStatusType {
-    Active = 'ACTIVE', // 활성 상태
-    Inactive = 'INACTIVE', // 비활성 상태
-    Deleted = 'DELETED', // 삭제 상태
-    Expired = 'EXPIRED', // 만료 상태
-}
--- src/payment-method/enum/payment-method-type.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum PaymentMethodType {
-    Card = 'CARD',
-    Bank = 'BANK',
-    Point = 'POINT',
-    Coupon = 'COUPON',
-    Etc = 'ETC',
-}
