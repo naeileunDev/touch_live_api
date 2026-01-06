58e5af3386e207879f5c5108d25be73cdc0b523d
rimhyena <rimhyena@naeileun.dev>
2026-01-06 16:16:59 +0900

    ✨feat: 주문 상품 상태 ENUM 업데이트 - 주문 상품 상태 ENUM에 교환 및 환불 관련 상태 추가 - 각 상태에 대한 주석 추가로 가독성 향상

--- src/order/enum/order-product-status.enum.ts
+++ src/order/enum/order-product-status.enum.ts
@@ -1,10 +1,14 @@
 export enum OrderProductStatus {
-    Pending = 'PENDING',
+    Pending = 'PENDING',  //장바구니 상태 
     PaymentFailed = 'PAYMENT_FAILED',
     Paid = 'PAID',
     Preparing = 'PREPARING',
-    Shipped = 'SHIPPED',
+    Shipped = 'SHIPPED', 
     Delivered = 'DELIVERED',
     ExchangeRequested = 'EXCHANGE_REQUESTED',
-    ReturnRequested = 'RETURN_REQUESTED',
+    ExchangeApproved = 'EXCHANGE_APPROVED',
+    ExchangeCompleted = 'EXCHANGE_COMPLETED',
+    RefundRequested = 'REFUND_REQUESTED',
+    RefundApproved = 'REFUND_APPROVED',
+    RefundCompleted = 'REFUND_COMPLETED',
 }
